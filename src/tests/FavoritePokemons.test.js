ebd0c8e8a55a9cbb66fc3875a6483206a0a80f3f
AmadeusDias <amadeusdias@hotmail.com>
2022-08-17 16:59:31 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import { userEvent } from '@testing-library/user-event';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente Favorites', () => {
+  test('Exibe a mensagem na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const menssage = screen.getByText('No favorite pokemon found');
+    expect(menssage).toBeInTheDocument();
+  });
+});
