3f41ba0d6500da171fc08fafb4ebe2ed078e66aa
AmadeusDias <amadeusdias@hotmail.com>
2022-08-17 16:11:47 -0300

    primeiro req

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import { userEvent } from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente App', () => {
+  test('Verificando os links', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    const favooritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favooritesLink).toBeInTheDocument();
+  });
+});
