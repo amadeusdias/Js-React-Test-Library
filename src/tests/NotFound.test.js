300965607e9660071e38ab43ebca13684100d27f
AmadeusDias <amadeusdias@hotmail.com>
2022-08-17 17:23:00 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import { userEvent } from '@testing-library/user-event';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente About', () => {
+  test('Verificando Tags simmples', () => {
+    renderWithRouter(<NotFound />);
+    // const theUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const title = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+    const img = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(title).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
